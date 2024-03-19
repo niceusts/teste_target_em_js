82ce7cd8071b5232bd530af4ff38c361a8d6fb59
Niceu Santos Biriba <51482169+niceusts@users.noreply.github.com>
2024-03-19 21:37:03 +0000

    questão 1

---
+++ questao1.js
@@ -0,0 +1,12 @@
+let INDICE = 13;
+let SOMA = 0;
+let K = 0;
+
+while (K < INDICE) {
+  K++;
+  SOMA = SOMA + K;
+        console.log("SOMA", SOMA);
+    console.log("K", K);
+}
+
+console.log(SOMA);
